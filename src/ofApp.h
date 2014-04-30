ab2c1ff625b0f0dfd1d454f502ee16590e534931
Naoki Endoh <naoki.endoh@gmail.com>
2014-04-30 13:57:43 +0900

    Draw video grabber

--- src/ofApp.h
+++ src/ofApp.h
@@ -17,4 +17,6 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
+    
+        ofVideoGrabber myGrab;
 };
